e26a0135ea62eeec9fbf8033b32a1dd47e89483f
Meivyn <793322+Meivyn@users.noreply.github.com>
2023-10-07 02:54:47 -0400

    Fix for game v1.32.0

--- Source/0_Harmony/VRControllerPatches/VRControllerGetControllerOffsetPatch.cs
+++ Source/0_Harmony/VRControllerPatches/VRControllerGetControllerOffsetPatch.cs
@@ -1,11 +1,18 @@
+using System.Reflection;
 using HarmonyLib;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.XR;
 
 namespace EasyOffset;
 
-[HarmonyPatch(typeof(VRController), nameof(VRController.TryGetControllerOffset))]
-internal static class VRControllerGetControllerOffsetPatch {
+[HarmonyPatch]
+internal static class VRControllerGetControllerOffsetPatch
+{
+    [UsedImplicitly]
+    private static MethodInfo TargetMethod() => AccessTools.Method(typeof(VRController), nameof(VRController.TryGetControllerOffset),
+        new[] { typeof(IVRPlatformHelper), typeof(VRControllerTransformOffset), typeof(XRNode), typeof(Pose).MakeByRefType() });
+
     [UsedImplicitly]
     private static bool Prefix(ref Pose poseOffset) {
         if (PluginConfig.IsDeviceless && !PluginConfig.EnabledForDeviceless) return true;
